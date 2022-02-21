53a49c3bf21ce0772c20c88160b9b1902974796f
Gino S <somiglianagino@gmail.com>
2022-02-21 20:17:12 -0300

    Update to new component manager

--- bot/helpers/channels.py
+++ bot/helpers/channels.py
@@ -1,13 +1,11 @@
-from telnetlib import GA
-from tokenize import String
-
-
-from lightbulb import BotApp
 import lightbulb
-from hikari import PermissionOverwrite, PermissionOverwriteType, Permissions, GatewayGuild, PartialRole, GuildChannel
+import hikari
+from . import components
+from hikari.impl.special_endpoints import ActionRowBuilder
+from hikari import ButtonStyle, PartialComponent, PermissionOverwrite, PermissionOverwriteType, Permissions, GatewayGuild, PartialRole, GuildTextChannel, embeds, ButtonComponent, ActionRowComponent, ComponentType
 
 
-async def create_channel(ctx: lightbulb.Context) -> GuildChannel:
+async def create_channel(ctx: lightbulb.Context) -> GuildTextChannel:
     guild: GatewayGuild = ctx.interaction.get_guild()
 
     everyoneRole = (await guild.fetch_roles())[0]
@@ -29,5 +27,12 @@ async def create_channel(ctx: lightbulb.Context) -> GuildChannel:
             | Permissions.SEND_MESSAGES
         )
     )]
-    created_channel = await guild.create_text_channel('secret', permission_overwrites=overwrite)
+    created_channel: GuildTextChannel = await guild.create_text_channel(ctx.options.category, permission_overwrites=overwrite)
     return created_channel
+
+
+async def send_ticket_message(ctx: lightbulb.Context, created_channel: GuildTextChannel) -> None:
+    row = components.ticket_close_button()
+    embed = embeds.Embed(title="New ticket", description="This is a new ticket")
+    await created_channel.send(embed=embed, components=[row])
+    return
